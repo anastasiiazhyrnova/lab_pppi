9c06e7cc9bfc2ae81914ee8fdda7972250d28307
Nastia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2023-08-23 15:16:17 +0300

    Added new file

---
+++ lab8/WebAPI_Labs/Services/PasswordService/PasswordService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace WebAPI_Labs.Services.PasswordService
+{
+    public class PasswordService : IPasswordService
+    {
+        private readonly int iterations = 10001;
+        public string HashPassword(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: iterations,
+                numBytesRequested: 256 / 8));
+
+            return $"{Convert.ToBase64String(salt)}.{hashed}";
+        }
+
+        public bool VerifyPassword(string enteredPassword, string storedHash)
+        {
+            var parts = storedHash.Split('.');
+            var salt = Convert.FromBase64String(parts[0]);
+            var hashedPassword = parts[1];
+
+            string enteredHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: enteredPassword,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA512,
+                iterationCount: iterations,
+                numBytesRequested: 256 / 8));
+
+            return enteredHash == hashedPassword;
+        }
+    }
+
+}
